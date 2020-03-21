bda42c642578eb8faa16dd68b1e61ef014484da6
Luis Otávio <luis.otavio2425@gmail.com>
2020-03-21 16:42:05 -0300

    Add loadable

--- src/routes.js
+++ src/routes.js
@@ -4,18 +4,45 @@ import Loadable from 'react-loadable';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 // import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
+// Import Includes
 import NavBar from './pages/include/Navbar';
 import Footer from './pages/include/Footer';
-import Index from './pages/Index';
-import SignIn from './pages/SignIn';
-import Quem from './pages/Quem';
-import OQue from './pages/OQue';
-import Como from './pages/Como';
-import Eventos from './pages/Eventos';
-
 import './global.css';
-
 import { isAuthenticated } from './auth.js';
+
+const Loading = () => {
+    return(
+        <div style={{ height: '100vh'}}>
+            <div className="loader"></div>
+        </div>
+    );
+};
+// Import Pages
+const Index = Loadable({
+    loader: () => import('./pages/Index'),
+    loading: Loading 
+});
+
+const Quem = Loadable({
+    loader: () => import('./pages/Quem'),
+    loading: Loading
+});
+
+const OQue = Loadable({
+    loader: () => import('./pages/OQue'),
+    loading: Loading
+});
+
+const Como = Loadable({
+    loader: () => import('./pages/Como'),
+    loading: Loading
+});
+
+const Eventos = Loadable({
+    loader: () => import('./pages/Eventos'),
+    loading: Loading
+});
+
 
 const PrivateRoute = ({ component: Component, ...rest}) => (
     <Route {...rest} render={props => (
@@ -25,12 +52,7 @@ const PrivateRoute = ({ component: Component, ...rest}) => (
             <Redirect to={{ pathname: '/', state: { from: props.location } }} />
         )
     )} />
-);
-
-
-const routes = [
-    { path: '/sis/login', name: 'Login', Component: SignIn, styles: { height: '100vh', justifyContent: 'center', padding: '0 20px' } },
-];
+);   
 
 
 const Routes = () => (
