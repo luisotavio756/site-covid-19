018ffc7a9beab3cbfc9102a6ab9c000ab2e6e4b0
Luis Otávio <luis.otavio2425@gmail.com>
2020-03-25 11:55:47 -0300

    First Commit

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,21 +1,15 @@
 import React from 'react';
 
-import Banner from './components/Banner';
-import Eventos from './components/Eventos';
-import Serviços from './components/Serviços';
-// import QuemSomos2 from './components/QuemSomos2';
-import Jobs from './components/Jobs';
-import Email from './components/Email';
+// import Banner from './components/Banner';
 
 export default function Index() {
 
     return (
         <>
-            <Banner />
-            <Eventos />
-            <Serviços />
-            <Jobs />
-            <Email />
+            <div className="teste" style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                <h1>Covid-19</h1>
+            </div>
+            {/* <Banner /> */}
         </>
     );
 }
