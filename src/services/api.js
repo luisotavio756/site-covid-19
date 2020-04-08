652b3e1144d87d1368509154c4444fe10e00cbc0
Luis Otávio <luis.otavio2425@gmail.com>
2020-04-08 19:59:22 -0300

    new commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
 const api = axios.create({
-    baseURL: "http://localhost:3333",
+    baseURL: "http://api-coronainfo.herokuapp.com/",
 });
 
 export default api;
